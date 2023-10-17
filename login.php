39b955479c6b1d82c6ecc03c50dd179f91f4d35a
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-10-17 10:55:44 -0300

    Correção feedback

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php 
 require_once "inc/cabecalho.php";
-use Microblog\{ControleDeAcesso,Usuario};
+use Microblog\{ControleDeAcesso,Usuario, Utilitarios};
 
 /* Programação das mensagens de feedback (campos obrigatórios, dados incorretos, saiu do sistema etc) */
 if(isset($_GET["campos_obrigatorios"])){
-	$feedback = "Você deve logar primeiro!";
+	$feedback = "Você deve preencher os dois campos!";
 } 
 ?>
 
@@ -38,6 +38,9 @@ if(isset($_POST['entrar'])){
 		$usuario = new Usuario;
 		$usuario->setEmail($_POST["email"]);
 		
+		/* Buscar o usuários/email no Banco de dados */
+		$dados = $usuario->buscar();
+		Utilitarios::dump($dados);
 
 
 	}

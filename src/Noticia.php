28086f0fb376d64fc9cda906971c2bbb580856ed
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-10-26 10:05:07 -0300

    loop para destaques

--- src/Noticia.php
+++ src/Noticia.php
@@ -153,7 +153,6 @@ final class Noticia
     }
 
 
-
     /* Método para upload de fotos */
 
     public function UploadFotos(array $arquivo): void
@@ -179,6 +178,21 @@ final class Noticia
 
         // Movemos/Enviamos  da área temporária para a final/destino
         move_uploaded_file($temporario, $pastaFinal);
+    }
+
+    /* Métodos da área pública */
+    public function listarDestaques():array{
+        $sql = "SELECT id,titulo,resumo,imagem FROM noticias WHERE destaque =:destaque ORDER BY data DESC";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":destaque",$this->destaque,PDO::PARAM_STR);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao Listar Destaques: ".$erro->getMessage());
+        }
+        return $resultado;
     }
 
     public function getId(): int

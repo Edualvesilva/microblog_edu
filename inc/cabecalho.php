3acc9f4f59d33be8710e1f39b8ab5622c767e502
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-10-31 09:46:16 -0300

    Ultimo update

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -55,13 +55,13 @@ $listaCategoria = $noticia->categoria->ler();
             </li>
           </ul>
 
-          <div class="postion-relative">
+          <div class="position-relative">
             <form autocomplete="off" class="d-flex" action="resultados.php" method="POST" onsubmit="return false" id="form-busca">
               <input id="campo-busca" name="busca" class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquise aqui">
 
-                  <div id="resultados" class="mt-3 position-absolute container bg-white shadow-lg p-3"></div>
-
+              
             </form>
+            <div id="resultados" class="mt-3 position-absolute container bg-white shadow-lg p-3"></div>
           </div>
         </div>
       </div>
--- resultados.php
+++ resultados.php
@@ -11,8 +11,8 @@ $resultado = $noticia->busca();
 $quantidade = count($resultado);
 
 if ($quantidade > 0) { ?>
-    <h2 class="fs-5">Resultados: <span><?= $quantidade ?></span></h2>
-    <div>
+    <h2 class="fs-5">Resultados: <span class="badge bg-success"><?= $quantidade ?></span></h2>
+    <div class="list-group">
         <?php foreach ($resultado as $itemNoticia) { ?>
             <a class="list-group-item list-group-item-action" href="noticia.php?id=<?= $itemNoticia["id"] ?>"><?= $itemNoticia["titulo"] ?></a>
         <?php } ?>
